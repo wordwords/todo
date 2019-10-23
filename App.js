db9ddfd2b29f5ba20d3218d07d8235e8535a4bc2
ggavi2000 <ggavi2000@naver.com>
2019-10-23 17:19:15 +0900

    동그라미 버튼 클릭 (완료/미완료)

--- App.js
+++ App.js
@@ -1,18 +1,52 @@
 import React from "react"
-import { StyleSheet, Text, View, TextInput, Dimensions } from "react-native"
+import { StyleSheet, Text, View, TextInput, Dimensions, ScrollView } from "react-native"
+import Todo from "./ToDo"
 
 // 기기 화면의 너비와 높이 얻어오기
 const { height, width } = Dimensions.get("window")
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}> To Do </Text>
-      <View style={styles.card}>
-        <TextInput style={styles.input} placeholder={"새로운 일정"} />
+export default class App extends React.Component {
+  state = {
+    newToDo: "",
+  }
+
+  render() {
+    const { newToDo } = this.state
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}> To Do </Text>
+        <View style={styles.card}>
+
+          <TextInput
+            style={styles.input}
+            placeholder={"새로운 일정"}
+            value={newToDo}
+            onChangeText={this._controllNewToDo}
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"} // 확인버튼이 '리턴' 버튼이면 '완료' 버튼으로 변경
+          />
+
+          <ScrollView contentContainerStyle={styles.toDos}>
+            <Todo />
+            <Todo />
+            <Todo />
+            <Todo />
+            <Todo />
+            <Todo />
+            <Todo />
+            <Todo />
+            <Todo />
+            <Todo />
+          </ScrollView>
+        </View>
       </View>
-    </View>
-  )
+    )
+  }
+  _controllNewToDo = (text) => {
+    this.setState({
+      newToDo: text,
+    })
+  }
 }
 
 const styles = StyleSheet.create({
@@ -37,6 +71,17 @@ const styles = StyleSheet.create({
     width: width - 25,
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
-    elevation: 5
+    elevation: 5,
   },
+
+  input: {
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 25,
+  },
+  
+  toDos: {
+    alignItems: "center"
+  }
 })
---
+++ ToDo.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react"
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native"
+
+const { width, height } = Dimensions.get("window")
+
+// 수정을 누르면 state를 수정모드로 변경해야 한다.
+export default class ToDo extends Component {
+  state = {
+    isEditing: false, // 수정할 때 쓰이는 state
+    isCompleted: false, // 수정을 안할 때 쓰이는 state (그냥 보여줄 때)
+  }
+
+  render() {
+    const { isCompleted } = this.state // 계획 완료? 미완료?
+
+    return (
+      // TouchableOpacity: 터치 이벤트(onPress 등)를 사용할 수 있는 View
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this._toggleComplete}>
+          <View style={[styles.circle, isCompleted ? styles.completedCircle : styles.uncompletedCircle]} />
+        </TouchableOpacity>
+        <Text style={styles.text}> 투두 </Text>
+      </View>
+    )
+  }
+
+  // 완성/미완성에 따라 다른 스타일을 주고 싶은 경우
+  _toggleComplete = () => {
+    this.setState((prevState) => {
+      return {
+        isCompleted: !prevState.isCompleted,
+      }
+    })
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: width - 50,
+    borderBottomColor: "#bbb", // 밑줄 색깔
+    borderBottomWidth: StyleSheet.hairlineWidth, // 밑에 밑줄
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  // 빨간색 원 (클릭할 수 있다)
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderColor: "red",
+    borderWidth: 3,
+    marginRight: 10,
+    marginLeft: 10,
+  },
+
+  text: {
+    fontWeight: "600",
+    fontSize: 20,
+    marginVertical: 20,
+  },
+
+  completedCircle: {
+    borderColor: "#bbb"
+  },
+
+  uncompletedCircle: {
+    borderColor: "#F23657"
+  }
+})
